995fcb4fee43a1187058e8be98994195dfd6cfee
Rue <rue.sriharsha@gmail.com>
2021-08-29 00:41:45 +0530

    Fixed login issues

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
-function setCookie(){
-    chrome.extension.sendMessage({name: 'setLoginCookie'}, function(otherResponse) {
-        console.log(otherResponse)
-    })
-}
-
-function getCookie(){
-    chrome.extension.sendMessage({name: 'getLoginCookie'}, function(response) {
-        alert(response)
-    })
-}
+document.body.onload = function() {
+    chrome.storage.sync.get("data", function(items) {
+      if (!chrome.runtime.error) {
+        console.log(items);
+        document.getElementById("data").innerText = items.data;
+      }
+    });
+  }
+  
+  document.getElementById("set").onclick = function() {
+    var d = document.getElementById("text").value;
+    chrome.storage.sync.set({ "data" : d }, function() {
+      if (chrome.runtime.error) {
+        console.log("Runtime error.");
+      }
+    });
+    window.close();
+  }
